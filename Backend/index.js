9f111e081837b4e988b68f062742b9cd3a4ec187
PRATIK KUMAR SAHOO <93589149+Pratik-k-sahoo@users.noreply.github.com>
2025-07-04 23:07:53 +0530

    Manual Header setting cancelled

--- Backend/index.js
+++ Backend/index.js
@@ -23,17 +23,6 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"Content-Type",
-		"Authorization"
-	);
-	next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
