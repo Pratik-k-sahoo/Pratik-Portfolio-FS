81ae85be8fb98db782d0390100342d8b8664fbdc
PRATIK KUMAR SAHOO <93589149+Pratik-k-sahoo@users.noreply.github.com>
2025-07-05 00:06:34 +0530

    Updated cors for domain change

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ import connectDB from "./config/connectDB.js";
 
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-	origin: "https://pratik-portfolio-fs-fe.vercel.app",
+	origin: "https://portfolio.sipun.online/",
 	methods: ["GET", "POST", "PUT", "DELETE"],
 	credentials: true,
 };
