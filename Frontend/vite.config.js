a0df5893aa656ada48498c0eaa23c0e2dcec96d4
PRATIK KUMAR SAHOO <93589149+Pratik-k-sahoo@users.noreply.github.com>
2025-07-04 15:58:14 +0530

    vite config updated for backend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,14 +4,14 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/v1': {
-        target: 'http://localhost:3000/api',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  plugins: [react(), svgr()],
-})
+	server: {
+		proxy: {
+			"/v1": {
+				target: "https://pratik-portfolio-fs-backend.onrender.com/api",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+	plugins: [react(), svgr()],
+});
